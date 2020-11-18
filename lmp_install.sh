4a14328fc385a75dd1d6a68d96620643113f7b0e
zhanggehui <395510548@qq.com>
2020-11-18 22:07:56 +0800

    update

--- lmp_install.sh
+++ lmp_install.sh
@@ -3,7 +3,7 @@
 export PATH=/appsnew/usr/cmake/cmake-3.14.3/bin:$PATH
 source /appsnew/source/intel2018.sh
 source /appsnew/source/zlib-1.2.11.sh
-#source /appsnew/source/Python-3.7.3.sh
+source /appsnew/source/Python-3.7.3.sh
 
 export CCFLAGS='-g -O3 -std=c++11'
 
