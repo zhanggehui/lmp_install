b3f3bcac26b570d67a6c78c074e6b4ee793ea0e9
zhanggehui <395510548@qq.com>
2020-11-19 18:56:24 +0800

    update

--- lammps_29Oct2020.sh
+++ lammps_29Oct2020.sh
@@ -2,19 +2,12 @@ export PATH=/appsnew/usr/cmake/cmake-3.14.3/bin:$PATH
 source /appsnew/source/intel2018.sh
 
 processor_num=`cat /proc/cpuinfo| grep "processor"| wc -l`
-if [ $# -ge 1 ]; then    
-    if [ $1 == 'cn_nl' ]; then
-        source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_install/lammps_cnnl.sh
-    elif [ $1 == 'cn-short' ] || [ $1 == 'cn-long' ]; then
-        source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_install/lammps_cns.sh
-    else
-        echo 'No such version! The defaul is cns version'
-        source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_install/lammps_cns.sh
-    fi
-else 
-    if [ ${processor_num} -eq 56 ]; then
-        source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_install/lammps_cnnl.sh
-    else
-        source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_install/lammps_cns.sh
-    fi
+if [ ${processor_num} -eq 56 ]; then
+    export PATH=/home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cnnl_install/bin:$PATH 
+    source /home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cnnl_build/etc/profile.d/lammps.sh
+    echo 'lammps cnnl version'
+else
+    export PATH=/home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cns_install/bin:$PATH 
+    source /home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cns_build/etc/profile.d/lammps.sh
+    echo 'lammps cns version'
 fi
