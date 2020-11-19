7287865fd3fd52d740d9d1128037770bc09d5523
zhanggehui <395510548@qq.com>
2020-11-19 15:43:47 +0800

    update

--- lammps_29Oct2020.sh
+++ lammps_29Oct2020.sh
@@ -2,26 +2,18 @@ export PATH=/appsnew/usr/cmake/cmake-3.14.3/bin:$PATH
 source /appsnew/source/intel2018.sh
 
 processor_num=`cat /proc/cpuinfo| grep "processor"| wc -l`
-if [ $# -ge 1 ] ; then    
-    if [ $1 == 'cnnl' ] ; then
-        export PATH=/home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cnnl_install/bin:$PATH 
-        source /home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cnnl_build/etc/profile.d/lammps.sh
-        echo 'lammps cnnl version'
-    elif [ $1 == 'cns' ] ; then
-        export PATH=/home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cns_install/bin:$PATH 
-        source /home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cns_build/etc/profile.d/lammps.sh
-        echo 'lammps cns version'
+if [ $# -ge 1 ]; then    
+    if [ $1 == 'cnnl' ]; then
+        source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_install/lammps_cnnl.sh
+    elif [ $1 == 'cns' ]; then
+        source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_install/lammps_cns.sh
     else
         echo 'No such version!'
     fi
 else 
-    if [ ${processor_num} -eq 56 ] ; then
-        export PATH=/home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cnnl_install/bin:$PATH 
-        source /home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cnnl_build/etc/profile.d/lammps.sh
-        echo 'lammps cnnl version'
+    if [ ${processor_num} -eq 56 ]; then
+        source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_install/lammps_cnnl.sh
     else
-        export PATH=/home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cns_install/bin:$PATH 
-        source /home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cns_build/etc/profile.d/lammps.sh
-        echo 'lammps cns version'
+        source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_install/lammps_cns.sh
     fi
 fi
---
+++ lammps_cns.sh
@@ -0,0 +1,3 @@
+export PATH=/home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cns_install/bin:$PATH 
+source /home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cns_build/etc/profile.d/lammps.sh
+echo 'lammps cns version'
