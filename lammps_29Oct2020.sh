d513ed06f4f533cc37372fa1f7c1c78efc477010
zhanggehui <395510548@qq.com>
2020-11-19 15:23:04 +0800

    update

--- lammps_29Oct2020.sh
+++ lammps_29Oct2020.sh
@@ -2,11 +2,11 @@ export PATH=/appsnew/usr/cmake/cmake-3.14.3/bin:$PATH
 source /appsnew/source/intel2018.sh
 
 processor_num=`cat /proc/cpuinfo| grep "processor"| wc -l`
-if [ ${processor_num} -eq 56 ] || [ [ $1 == 'cnnl' ] && [ $1 != 'cns' ] ] ; then
+if [ ${processor_num} -eq 56 ] || [ $1 == 'cnnl' ] ; then
     export PATH=/home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cnnl_install/bin:$PATH 
     source /home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cnnl_build/etc/profile.d/lammps.sh
     echo 'lammps cnnl version'
-else
+elif [ ${processor_num} -eq 28 ] || [ $1 == 'cns' ]
     export PATH=/home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cns_install/bin:$PATH 
     source /home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cns_build/etc/profile.d/lammps.sh
     echo 'lammps cns version'
