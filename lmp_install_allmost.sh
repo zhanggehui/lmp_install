4b645f332d599ffbe604216cab2cf825050aa045
zhanggehui <395510548@qq.com>
2020-11-19 13:48:41 +0800

    update

--- lmp_install_allmost.sh
+++ lmp_install_allmost.sh
@@ -36,7 +36,7 @@ cmake -DBUILD_MPI=yes -DBUILD_OMP=yes  \
 -DPKG_USER-SDPD=yes -DPKG_USER-SMTBQ=yes -DPKG_USER-SPH=yes \
 -DPKG_USER-TALLY=yes -DPKG_USER-UEF=yes -DPKG_USER-YAFF=yes \
 \
--DCMAKE_INSTALL_PREFIX=../install \
+-DCMAKE_INSTALL_PREFIX=../cnnl_almost \
 ../cmake && make -j10 && make install
 
 deactivate
