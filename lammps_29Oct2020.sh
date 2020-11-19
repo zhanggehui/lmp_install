7446a74977cee2ccb3f5a4a8226543738fa0245e
zhanggehui <395510548@qq.com>
2020-11-19 17:46:40 +0800

    update

--- lammps_29Oct2020.sh
+++ lammps_29Oct2020.sh
@@ -5,10 +5,11 @@ processor_num=`cat /proc/cpuinfo| grep "processor"| wc -l`
 if [ $# -ge 1 ]; then    
     if [ $1 == 'cn_nl' ]; then
         source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_install/lammps_cnnl.sh
-    elif [ $1 == 'cn-short' ]; then
+    elif [ $1 == 'cn-short' ] || [ $1 == 'cn-long' ]; then
         source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_install/lammps_cns.sh
     else
-        echo 'No such version!'
+        echo 'No such version! The defaul is cns version'
+        source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_install/lammps_cns.sh
     fi
 else 
     if [ ${processor_num} -eq 56 ]; then
