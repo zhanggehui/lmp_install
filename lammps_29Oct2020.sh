6168ff7b10305a367848c5b8522a37b0fb792883
zhanggehui <395510548@qq.com>
2020-11-19 15:23:19 +0800

    update

--- lammps_29Oct2020.sh
+++ lammps_29Oct2020.sh
@@ -6,7 +6,7 @@ if [ ${processor_num} -eq 56 ] || [ $1 == 'cnnl' ] ; then
     export PATH=/home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cnnl_install/bin:$PATH 
     source /home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cnnl_build/etc/profile.d/lammps.sh
     echo 'lammps cnnl version'
-elif [ ${processor_num} -eq 28 ] || [ $1 == 'cns' ]
+elif [ ${processor_num} -eq 40 ] || [ $1 == 'cns' ]
     export PATH=/home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cns_install/bin:$PATH 
     source /home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cns_build/etc/profile.d/lammps.sh
     echo 'lammps cns version'
