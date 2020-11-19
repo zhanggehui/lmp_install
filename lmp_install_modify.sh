b171a4713fb4d42eb07070bce0e4599b62638322
zhanggehui <395510548@qq.com>
2020-11-19 19:58:45 +0800

    update

--- lmp_install_modify.sh
+++ lmp_install_modify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /home/liufeng_pkuhpc/lustre2/lammps_modify/build_modify
-rm -rf *
-rm -rf ../install_modify
+cd /home/liufeng_pkuhpc/lustre2/lammps_modify/build_modify && \
+rm -rf * && rm -rf ../install_modify
+
 mv /home/liufeng_pkuhpc/lustre2/lammps_modify/src/MC/lmp_newatomswap/fix* \
 /home/liufeng_pkuhpc/lustre2/lammps_modify/src/MC/
 
