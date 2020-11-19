7513b1c320fabb3ec6b06762b8eb259272b7707e
zhanggehui <395510548@qq.com>
2020-11-19 13:35:35 +0800

    update

---
+++ environment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export PATH=/appsnew/usr/cmake/cmake-3.14.3/bin:$PATH
+source /appsnew/source/intel2018.sh
+source /appsnew/source/zlib-1.2.11.sh
+source /appsnew/source/Python-3.7.3.sh
+export CCFLAGS='-g -O3 -std=c++11'
---
+++ lammps_29Oct2020.sh
@@ -0,0 +1,6 @@
+processor_num=`cat /proc/cpuinfo| grep "processor"| wc -l`
+if [ ${processor_num} -eq 56 ] ; then
+    source 
+else
+    source 
+fi
