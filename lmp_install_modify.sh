9335cabebaacfd70480809f98534c72e7ad047c5
zhanggehui <395510548@qq.com>
2020-11-19 19:42:06 +0800

    update

---
+++ lmp_install_modify.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mv /home/liufeng_pkuhpc/lustre2/lammps_modify/src/MC/lmp_newatomswap/fix* ../
+rm -rf /home/liufeng_pkuhpc/lustre2/lammps_modify/src/MC/lmp_newatomswap
+
+#####
+export PATH=/appsnew/usr/cmake/cmake-3.14.3/bin:$PATH
+source /appsnew/source/intel2018.sh
+source /appsnew/source/zlib-1.2.11.sh
+source /appsnew/source/Python-3.7.3.sh
+export CCFLAGS='-g -O3 -std=c++11'
+
+# 42User_package 33Standard_package
+cmake -DBUILD_MPI=yes -DBUILD_OMP=yes  \
+-DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc -DCMAKE_Fortran_COMPILER=ifort \
+-DMPI_C_COMPILER=mpiicc -DMPI_CXX_COMPILER=mpiicpc \
+\
+-DFFT=mkl \
+-DPKG_MC=yes \
+-DCMAKE_INSTALL_PREFIX=../install_modify \
+../cmake && make -j10 && make install
+
+deactivate
+
+currentdir=`pwd` 
+cd /home/liufeng_pkuhpc/lustre2/lammps_modify/src/MC
+git clone https://github.com/zhanggehui/lmp_newatomswap.git
+cd $currentdir
