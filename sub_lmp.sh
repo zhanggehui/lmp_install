edd2ba59d88e0a99627af5ab308402dc1ea501cf
zhanggehui <395510548@qq.com>
2020-11-20 15:26:55 +0800

    update

--- sub_lmp.sh
+++ sub_lmp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J lmp
+#SBATCH -J lmp_
 #SBATCH -p cn-long
 #SBATCH -N 4
 #SBATCH --qos=liufengcnl
@@ -13,6 +13,6 @@
 hosts=`scontrol show hostname $SLURM_JOB_NODELIST`; echo $hosts
 
 source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_install/lammps_29Oct2020.sh
-#source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_install/lmp_install_modify.sh
+#source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_install/lmp_install_modify.sh   #cns version
 
 mpirun -np $SLURM_NTASKS lmp -in step4.2_annealing.inp
