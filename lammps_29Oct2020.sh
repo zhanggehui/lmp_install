9dcae41ffa3a5e33c9e451b8caf796860879d883
zhanggehui <395510548@qq.com>
2020-11-21 16:58:07 +0800

    update

--- lammps_29Oct2020.sh
+++ lammps_29Oct2020.sh
@@ -1,8 +1,8 @@
 source /appsnew/source/intel2018.sh
 
 processor_num=`cat /proc/cpuinfo| grep "processor"| wc -l`
-echo $processor_num
-if [ ${processor_num} -eq 56 ]; then
+#echo $processor_num
+if [ ${processor_num} -eq 56 ] || [ ${processor_num} -eq 28 ]; then
     export PATH=/home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cnnl_install/bin:$PATH 
     source /home/liufeng_pkuhpc/lustre2/29Oct2020_lammps/build_almost/cnnl_build/etc/profile.d/lammps.sh
     echo 'lammps cnnl version'
