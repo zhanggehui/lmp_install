24c60676c6d29a1a9ee01fca97b9a6600a8825f0
zhanggehui <395510548@qq.com>
2021-01-04 16:50:49 +0800

    update

--- lammps_modify.sh
+++
@@ -1,5 +0,0 @@
-source /appsnew/source/intel2018.sh
-
-export PATH=/home/liufeng_pkuhpc/lustre2/lammps_modify/install_modify/bin:$PATH 
-source /home/liufeng_pkuhpc/lustre2/lammps_modify/build_modify/etc/profile.d/lammps.sh
-echo 'lammps modify version'
