69551d20735605806d669132f6c8930c238432bf
zhanggehui <395510548@qq.com>
2021-01-08 20:14:37 +0800

    update

--- lammps_29Oct2020.sh
+++ lammps_29Oct2020.sh
@@ -1,17 +1,18 @@
 source /appsnew/source/intel2018.sh
-
 processor_num=`cat /proc/cpuinfo| grep "processor"| wc -l`
-
 if [ $# -ge 1 ] &&  [ $1 != auto ]; then
     if [ $1 == cnnl ]; then
-        source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_use/lammps_cnnl.sh
+        cnword=cnnl
     else
-        source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_use/lammps_cns.sh
+        cnword=cns
     fi
 else
     if [ ${processor_num} -eq 56 ] || [ ${processor_num} -eq 28 ]; then
-        source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_use/lammps_cnnl.sh
+        cnword=cnnl
     else
-        source /home/liufeng_pkuhpc/lustre2/zgh/zgh_lmp/lmp_use/lammps_cns.sh
+        cnword=cns
     fi
 fi
+export PATH=/home/liufeng_pkuhpc/lustre2/lammps-29Oct20/static_build/"$cnword"_install/bin:$PATH 
+source /home/liufeng_pkuhpc/lustre2/lammps-29Oct20/static_build/"$cnword"_build/etc/profile.d/lammps.sh
+echo "lammps $word version"
